c848b16d3427e45d8fd30f66478d67d5992c6e1e
RujaSubba <rujasubba123@gmail.com>
2024-09-10 22:22:28 +0930

    project initialized and basic setup done

--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import "../styles/herosection.css"
+
 
 function HeroSection(){
     return(
-        <div>Hi this is the hero section of this project.</div>
+        <React.Fragment>
+            <div className="header">
+                 <div className="logo">
+                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3pUdXVG1y-3M3o5wSzXZwJ5Snx3Ct4g42Dg&s" alt="airbnb" />
+                 </div>
+                 <div className="header-centre">
+                     <input type="text" />
+                 </div>
+                 <div>
+                     <p>Airbnb your home</p>
+                     <img src="" alt="" />
+                
+                 </div>
+
+            </div>
+            
+
+        </React.Fragment>
+        
     )
 }
 export default HeroSection;
